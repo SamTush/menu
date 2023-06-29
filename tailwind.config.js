562e44f39c4ec7c6b05667fd9559c8133ffe26ea
Samson Tush <samo4421@icloud.com>
2023-06-29 21:28:20 +0300

    Add layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'pancake': "url('https://simplyhomecooked.com/wp-content/uploads/2022/08/buttermilk-pancakes-2.jpg')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
